24d6b543b9b8775dcb678e74e09cb44eb52dc525
OmegaDiamondX <92906797+OmegaDiamondX@users.noreply.github.com>
2024-09-24 22:05:54 +0300

    Add Task 2

--- Lab2/Task2/Form1.Designer.cs
+++ Lab2/Task2/Form1.Designer.cs
@@ -28,13 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(231, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(824, 530);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 507);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Брезенхем",
+            "Ву"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 90);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Lab2/Task2/Form1.cs
+++ Lab2/Task2/Form1.cs
@@ -4,17 +4,164 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Task2
 {
     public partial class Form1 : Form
     {
+        Bitmap drawboard;
+        private Point startPoint;
+        private bool isDrawing;
+        private Color selectedC;
+
         public Form1()
         {
             InitializeComponent();
+            drawboard = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = drawboard;
+            ClearBitmap();
+            selectedC = Color.Black;
+            button2.BackColor = selectedC;
+            comboBox1.SelectedIndex = 0;
+            isDrawing = false;
+        }
+
+        private void DrawBres(int x0,int x1,int y0,int y1, Color color)
+        {
+            int dx = Math.Abs(x1 - x0);
+            int dy = Math.Abs(y1 - y0);
+            int sx = (x0 < x1) ? 1 : -1;
+            int sy = (y0 < y1) ? 1 : -1;
+            int err = dx - dy;
+
+            while (true)
+            {
+                drawboard.SetPixel(x0, y0, color);
+
+                if (x0 == x1 && y0 == y1)
+                    break;
+
+                int e2 = 2 * err;
+                if (e2 > -dy)
+                {
+                    err -= dy;
+                    x0 += sx;
+                }
+                if (e2 < dx)
+                {
+                    err += dx;
+                    y0 += sy;
+                }
+            }
+        }
+
+        private void DrawWu(int x0, int x1, int y0, int y1, Color color)
+        {
+            bool steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
+            if (steep)
+            {
+                int temp;
+                temp = x0; x0 = y0; y0 = temp;
+                temp = x1; x1 = y1; y1 = temp;
+            }
+            if (x0 > x1)
+            {
+                int temp;
+                temp = x0; x0 = x1; x1 = temp;
+                temp = y0; y0 = y1; y1 = temp;
+            }
+
+            int dx = x1 - x0;
+            int dy = y1 - y0;
+            float gradient = (float)dy / (float)dx;
+
+            float y = y0 + gradient;
+
+            for (int x = x0 + 1; x <= x1; x++)
+            {
+                int yInt = (int)y;
+                float yFraction = y - yInt;
+
+                if (steep)
+                {
+                    SetPixelWithAlpha(yInt, x, 1 - yFraction,color);
+                    SetPixelWithAlpha(yInt + 1, x, yFraction,color);
+                }
+                else
+                {
+                    SetPixelWithAlpha(x, yInt, 1 - yFraction,color);
+                    SetPixelWithAlpha(x, yInt + 1, yFraction,color);
+                }
+
+                y += gradient;
+            }
+        }
+
+        private void SetPixelWithAlpha(int x, int y, float alpha, Color c)
+        {
+            if (x < 0 || x >= drawboard.Width || y < 0 || y >= drawboard.Height)
+                return;
+
+            Color existingColor = drawboard.GetPixel(x, y);
+            int newR = (int)(existingColor.R * (1 - alpha) + c.R * alpha);
+            int newG = (int)(existingColor.G * (1 - alpha) + c.G * alpha);
+            int newB = (int)(existingColor.B * (1 - alpha) + c.B * alpha);
+
+            drawboard.SetPixel(x, y, Color.FromArgb(newR, newG, newB));
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (!isDrawing)
+            {
+                // Первое нажатие: устанавливаем начальную точку
+                startPoint = e.Location;
+                isDrawing = true;
+            }
+            else
+            {
+                // Второе нажатие: устанавливаем конечную точку и рисуем линию
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    DrawBres(startPoint.X, e.X, startPoint.Y, e.Y, selectedC);
+                }
+                else { DrawWu(startPoint.X, e.X, startPoint.Y, e.Y, selectedC); };
+                pictureBox1.Invalidate(); // Обновляем PictureBox
+                isDrawing = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClearBitmap();
+        }
+
+        private void ClearBitmap()
+        {
+            using (Graphics g = Graphics.FromImage(drawboard))
+            {
+                g.Clear(Color.White);
+            }
+            pictureBox1.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (ColorDialog colorDialog = new ColorDialog())
+            {
+                colorDialog.Color = selectedC; // Устанавливаем текущий выбранный цвет
+
+                // Показываем диалог и проверяем, был ли выбран цвет
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedC = colorDialog.Color; // Сохраняем выбранный цвет
+                }
+            }
         }
     }
 }
