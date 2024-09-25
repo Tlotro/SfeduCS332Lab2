5b0907faa76fda09417d18d7fc84d768b1a1a52c
Tlotro <misha.pletneow@yandex.ru>
2024-09-25 14:30:40 +0300

    Upload

--- Lab2/Task1/Form1.Designer.cs
+++ Lab2/Task1/Form1.Designer.cs
@@ -28,13 +28,208 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.ColorButton = new System.Windows.Forms.Button();
+            this.ActionBox = new System.Windows.Forms.ComboBox();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.LoadFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.LoadPatternDialog = new System.Windows.Forms.OpenFileDialog();
+            this.LoadPatternButton = new System.Windows.Forms.Button();
+            this.pictureBoxPattern = new System.Windows.Forms.PictureBox();
+            this.NewCanvasButton = new System.Windows.Forms.Button();
+            this.NewCanvasY = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NewCanvasX = new System.Windows.Forms.TextBox();
+            this.PenSizeBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPattern)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(960, 540);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.AnyColor = true;
+            // 
+            // ColorButton
+            // 
+            this.ColorButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ColorButton.BackColor = System.Drawing.Color.Black;
+            this.ColorButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ColorButton.Location = new System.Drawing.Point(978, 39);
+            this.ColorButton.Name = "ColorButton";
+            this.ColorButton.Size = new System.Drawing.Size(120, 23);
+            this.ColorButton.TabIndex = 1;
+            this.ColorButton.UseVisualStyleBackColor = false;
+            this.ColorButton.Click += new System.EventHandler(this.ColorButton_Click);
+            // 
+            // ActionBox
+            // 
+            this.ActionBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ActionBox.FormattingEnabled = true;
+            this.ActionBox.Items.AddRange(new object[] {
+            "Draw",
+            "FillColor",
+            "FillImage",
+            "DrawBorder"});
+            this.ActionBox.Location = new System.Drawing.Point(978, 12);
+            this.ActionBox.Name = "ActionBox";
+            this.ActionBox.Size = new System.Drawing.Size(120, 21);
+            this.ActionBox.TabIndex = 2;
+            this.ActionBox.Text = "Draw";
+            this.ActionBox.SelectedIndexChanged += new System.EventHandler(this.ActionBox_SelectedIndexChanged);
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoadButton.Location = new System.Drawing.Point(979, 528);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(119, 23);
+            this.LoadButton.TabIndex = 3;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // LoadFileDialog
+            // 
+            this.LoadFileDialog.FileName = "openFileDialog1";
+            // 
+            // LoadPatternDialog
+            // 
+            this.LoadPatternDialog.FileName = "openFileDialog2";
+            // 
+            // LoadPatternButton
+            // 
+            this.LoadPatternButton.Enabled = false;
+            this.LoadPatternButton.Location = new System.Drawing.Point(978, 39);
+            this.LoadPatternButton.Name = "LoadPatternButton";
+            this.LoadPatternButton.Size = new System.Drawing.Size(120, 23);
+            this.LoadPatternButton.TabIndex = 4;
+            this.LoadPatternButton.Text = "Load Pattern";
+            this.LoadPatternButton.UseVisualStyleBackColor = true;
+            this.LoadPatternButton.Visible = false;
+            this.LoadPatternButton.Click += new System.EventHandler(this.LoadPatternButton_Click);
+            // 
+            // pictureBoxPattern
+            // 
+            this.pictureBoxPattern.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxPattern.Enabled = false;
+            this.pictureBoxPattern.Location = new System.Drawing.Point(979, 68);
+            this.pictureBoxPattern.Name = "pictureBoxPattern";
+            this.pictureBoxPattern.Size = new System.Drawing.Size(119, 67);
+            this.pictureBoxPattern.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxPattern.TabIndex = 5;
+            this.pictureBoxPattern.TabStop = false;
+            this.pictureBoxPattern.Visible = false;
+            // 
+            // NewCanvasButton
+            // 
+            this.NewCanvasButton.Location = new System.Drawing.Point(979, 499);
+            this.NewCanvasButton.Name = "NewCanvasButton";
+            this.NewCanvasButton.Size = new System.Drawing.Size(119, 23);
+            this.NewCanvasButton.TabIndex = 6;
+            this.NewCanvasButton.Text = "New canvas";
+            this.NewCanvasButton.UseVisualStyleBackColor = true;
+            this.NewCanvasButton.Click += new System.EventHandler(this.NewCanvasButton_Click);
+            // 
+            // NewCanvasY
+            // 
+            this.NewCanvasY.Location = new System.Drawing.Point(997, 473);
+            this.NewCanvasY.Name = "NewCanvasY";
+            this.NewCanvasY.Size = new System.Drawing.Size(101, 20);
+            this.NewCanvasY.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(978, 476);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Y";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(978, 450);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "X";
+            // 
+            // NewCanvasX
+            // 
+            this.NewCanvasX.Location = new System.Drawing.Point(997, 447);
+            this.NewCanvasX.Name = "NewCanvasX";
+            this.NewCanvasX.Size = new System.Drawing.Size(101, 20);
+            this.NewCanvasX.TabIndex = 9;
+            // 
+            // PenSizeBox
+            // 
+            this.PenSizeBox.Location = new System.Drawing.Point(979, 68);
+            this.PenSizeBox.Name = "PenSizeBox";
+            this.PenSizeBox.Size = new System.Drawing.Size(100, 20);
+            this.PenSizeBox.TabIndex = 11;
+            this.PenSizeBox.Text = "1";
+            this.PenSizeBox.TextChanged += new System.EventHandler(this.PenSizeBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1110, 564);
+            this.Controls.Add(this.PenSizeBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NewCanvasX);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NewCanvasY);
+            this.Controls.Add(this.NewCanvasButton);
+            this.Controls.Add(this.pictureBoxPattern);
+            this.Controls.Add(this.LoadPatternButton);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.ActionBox);
+            this.Controls.Add(this.ColorButton);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPattern)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button ColorButton;
+        private System.Windows.Forms.ComboBox ActionBox;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.OpenFileDialog LoadFileDialog;
+        private System.Windows.Forms.OpenFileDialog LoadPatternDialog;
+        private System.Windows.Forms.Button LoadPatternButton;
+        private System.Windows.Forms.PictureBox pictureBoxPattern;
+        private System.Windows.Forms.Button NewCanvasButton;
+        private System.Windows.Forms.TextBox NewCanvasY;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NewCanvasX;
+        private System.Windows.Forms.TextBox PenSizeBox;
     }
 }
 
--- Lab2/Task1/Form1.cs
+++ Lab2/Task1/Form1.cs
@@ -4,17 +4,280 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Task1
 {
+    class CustomPointComparer : IComparer<Point>
+    {
+        public int Compare(Point x, Point y)
+        {
+            if (x.X == y.X && x.Y == y.Y)
+                return 0;
+
+            if (x.Y > y.Y || (x.Y==y.Y && x.X > y.X))
+                return 1;
+            else
+            return -1;
+        }
+    }
     public partial class Form1 : Form
     {
+        Bitmap BMP;
+        Bitmap PBMP;
+        int penradius = 1;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void ActionBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (((ComboBox)sender).SelectedIndex)
+            {
+                case 0:
+                    ColorButton.Enabled = true;
+                    ColorButton.Visible = true;
+                    pictureBoxPattern.Enabled = false;
+                    pictureBoxPattern.Visible = false;
+                    LoadPatternButton.Enabled = false;
+                    LoadPatternButton.Visible = false;
+                    PenSizeBox.Enabled = true;
+                    PenSizeBox.Visible = true;
+                    break;
+                case 1:
+                    ColorButton.Enabled = true;
+                    ColorButton.Visible = true;
+                    pictureBoxPattern.Enabled = false;
+                    pictureBoxPattern.Visible = false;
+                    LoadPatternButton.Enabled = false;
+                    LoadPatternButton.Visible = false;
+                    PenSizeBox.Enabled = false;
+                    PenSizeBox.Visible = false;
+                    break;
+                case 2:
+                    LoadPatternButton.Enabled = true;
+                    LoadPatternButton.Visible = true;
+                    pictureBoxPattern.Enabled = true;
+                    pictureBoxPattern.Visible = true;
+                    ColorButton.Enabled = false;
+                    ColorButton.Visible = false;
+                    PenSizeBox.Enabled = false;
+                    PenSizeBox.Visible = false;
+                    break;
+                case 3:
+                    ColorButton.Enabled = true;
+                    ColorButton.Visible = true;
+                    pictureBoxPattern.Enabled = false;
+                    pictureBoxPattern.Visible = false;
+                    LoadPatternButton.Enabled = false;
+                    LoadPatternButton.Visible = false;
+                    PenSizeBox.Enabled = false;
+                    PenSizeBox.Visible = false;
+                    break;
+            }
+        }
+
+        private void ColorButton_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            ColorButton.BackColor = colorDialog1.Color;
+            ColorButton.ForeColor = colorDialog1.Color;
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            LoadFileDialog.ShowDialog();
+            BMP = new Bitmap(LoadFileDialog.FileName);
+            pictureBox1.Image = BMP;
+        }
+
+        private void LoadPatternButton_Click(object sender, EventArgs e)
+        {
+            LoadPatternDialog.ShowDialog();
+            PBMP = new Bitmap(LoadPatternDialog.FileName);
+            pictureBoxPattern.Image = PBMP;
+        }
+
+        private void DrawLine(int X, int Y, Color filledColor)
+        {
+            int x = X+1;
+            int y = Y;
+            while (x < BMP.Width && BMP.GetPixel(x, y) == filledColor)
+            {
+                BMP.SetPixel(x, y, colorDialog1.Color);
+                x++;
+            }
+            int limright = x-1;
+            x = X;
+            while (x >= 0 && BMP.GetPixel(x, y) == filledColor)
+            {
+                BMP.SetPixel(x, y, colorDialog1.Color);
+                x--;
+            }
+            int limleft = x+1;
+            for (int i = limleft; i <= limright; i++)
+            {
+                if (y-1 >= 0 && BMP.GetPixel(i,y-1)==filledColor)
+                    DrawLine(i, y-1, filledColor);
+                if (y+1 < BMP.Height && BMP.GetPixel(i, y + 1) == filledColor)
+                    DrawLine(i, y+1, filledColor);
+            }
+        }
+
+        private void DrawPattern(int X, int Y, int SHX, int SHY, Color filledColor)
+        {
+            int x = X + 1;
+            int y = Y;
+            while (x < BMP.Width && BMP.GetPixel(x, y) == filledColor)
+            {
+                BMP.SetPixel(x, y, PBMP.GetPixel((x-SHX+((SHX-x)/PBMP.Width+1)*PBMP.Width) % PBMP.Width, (y-SHY + ((SHY - y) / PBMP.Height + 1) * PBMP.Height) % PBMP.Height));
+                x++;
+            }
+            int limright = x - 1;
+            x = X;
+            while (x >= 0 && BMP.GetPixel(x, y) == filledColor)
+            {
+                BMP.SetPixel(x, y, PBMP.GetPixel((x-SHX+ ((SHX - x) / PBMP.Width + 1) * PBMP.Width) % PBMP.Width, (y - SHY + ((SHY - y) / PBMP.Height + 1) * PBMP.Height) % PBMP.Height));
+                x--;
+            }
+            int limleft = x + 1;
+            for (int i = limleft; i <= limright; i++)
+            {
+                if (y - 1 >= 0 && BMP.GetPixel(i, y - 1) == filledColor && PBMP.GetPixel((i - SHX+ ((SHX - i) / PBMP.Width + 1) * PBMP.Width) % PBMP.Width, (y - SHY - 1 + ((SHY - y) / PBMP.Height + 1) * PBMP.Height) % PBMP.Height) != filledColor)
+                    DrawPattern(i, y - 1, SHX, SHY, filledColor);
+                if (y + 1 < BMP.Height && BMP.GetPixel(i, y + 1) == filledColor && PBMP.GetPixel((i - SHX+ ((SHX - i) / PBMP.Width + 1) * PBMP.Width) % PBMP.Width, (y - SHY + 1 + ((SHY - y) / PBMP.Height + 1) * PBMP.Height) % PBMP.Height) != filledColor)
+                    DrawPattern(i, y + 1, SHX, SHY, filledColor);
+            }
+        }
+
+        private void PutTheDamnedPointIntoTheDamnedListFunction(List<Point> DamnedList,Point DamnedPoint)
+        {
+            CustomPointComparer comparer = new CustomPointComparer();
+            int i = DamnedList.BinarySearch(DamnedPoint,comparer); 
+            if (i < 0) i = ~i;
+            DamnedList.Insert(i, DamnedPoint);
+        }
+
+        private List<Point> GetOutline(int X, int Y)
+        {
+            Point[] shifts = { new Point(0, 1), new Point(1, 1), new Point(1, 0), new Point(1, -1), new Point(0, -1), new Point(-1, -1), new Point(-1, 0), new Point(-1, 1) };
+            Color innerColor = BMP.GetPixel(X, Y);
+            while (BMP.GetPixel(X, Y) == innerColor)
+            {
+                if (X > BMP.Width)
+                    return null;
+                X++;
+            }
+            int startX = X;
+            int startY = Y;
+            Color borderColor = BMP.GetPixel(X, Y);
+            List<Point> res = new List<Point>();
+            res.Add(new Point(X, Y));
+            int direction = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                Point CP = shifts[(i + direction) % 8];
+                if (BMP.GetPixel(X + CP.X, Y + CP.Y) == borderColor)
+                {
+                    PutTheDamnedPointIntoTheDamnedListFunction(res, new Point(X + CP.X, Y + CP.Y));
+                    X += CP.X;
+                    Y += CP.Y;
+                    direction = (direction + 6 + i) % 8;
+                    break;
+                }
+            }
+            while (X != startX || Y != startY)
+            for (int i = 0; i < 8; i++)
+            {
+                Point CP = shifts[(i+direction)%8];
+                if (BMP.GetPixel(X+CP.X,Y+CP.Y)==borderColor)
+                {
+                    PutTheDamnedPointIntoTheDamnedListFunction(res, new Point(X + CP.X, Y + CP.Y));
+                    X += CP.X;
+                    Y+= CP.Y;
+                    direction = (direction + 6+i) % 8;
+                    break;
+                }
+            }
+
+            return res;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1.Image != null && (ActionBox.SelectedIndex > 0))
+            {
+                Point mousepos = GetCoords(e.Location.X, e.Location.Y);
+                if (mousepos.X < BMP.Width && mousepos.X >= 0 && mousepos.Y < BMP.Height && mousepos.Y >= 0)
+                {
+                    Color filledColor = BMP.GetPixel(mousepos.X, mousepos.Y);
+                    switch (ActionBox.SelectedIndex)
+                    {
+                        case 1:
+                            DrawLine(mousepos.X, mousepos.Y, filledColor);
+                        break;
+                        case 2:
+                            DrawPattern(mousepos.X, mousepos.Y, (mousepos.X+PBMP.Width/2)% PBMP.Width, (mousepos.Y+PBMP.Height/2)%PBMP.Height, filledColor);
+                        break;
+                        case 3:
+                            foreach (Point p in GetOutline(mousepos.X, mousepos.Y))
+                                BMP.SetPixel(p.X, p.Y, colorDialog1.Color);
+                        break;
+                    }
+                    pictureBox1.Image = BMP;
+                }
+            }
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (ActionBox.SelectedIndex == 0 && (Control.MouseButtons & MouseButtons.Left) != 0 )
+            {
+                Point mousepos = GetCoords(e.Location.X, e.Location.Y);
+                if (mousepos.X < BMP.Width && mousepos.X >= 0 && mousepos.Y < BMP.Height && mousepos.Y >= 0)
+                    for (int i = Math.Max(0, mousepos.X-penradius+1); i <= Math.Min(BMP.Width,mousepos.X+penradius-1); i++)
+                        for (int j = Math.Max(0, mousepos.Y - penradius + 1); j <= Math.Min(BMP.Height, mousepos.Y + penradius - 1); j++)
+                            BMP.SetPixel(i, j, colorDialog1.Color);
+                pictureBox1.Image = BMP;
+            }
+        }
+
+        private Point GetCoords(int mouseX, int mouseY)
+        {
+            int realW = pictureBox1.Image.Width;
+            int realH = pictureBox1.Image.Height;
+            int currentW = pictureBox1.ClientRectangle.Width;
+            int currentH = pictureBox1.ClientRectangle.Height;
+            double zoomW = (currentW / (double)realW);
+            double zoomH = (currentH / (double)realH);
+            double zoomActual = Math.Min(zoomW, zoomH);
+            double padX = zoomActual == zoomW ? 0 : (currentW - (zoomActual * realW)) / 2;
+            double padY = zoomActual == zoomH ? 0 : (currentH - (zoomActual * realH)) / 2;
+
+            int realX = (int)((mouseX - padX) / zoomActual);
+            int realY = (int)((mouseY - padY) / zoomActual);
+            return new Point(realX, realY);
+        }
+
+        private void NewCanvasButton_Click(object sender, EventArgs e)
+        {
+            int X, Y;
+            if (int.TryParse(NewCanvasX.Text,out X) && int.TryParse(NewCanvasY.Text, out Y))
+            {
+                BMP = new Bitmap(X,Y);
+                for (int i = 0; i < X; i++) 
+                    for (int j = 0; j < Y; j++)
+                        BMP.SetPixel(i, j, Color.White);
+                pictureBox1.Image = BMP;
+            }
+        }
+
+        private void PenSizeBox_TextChanged(object sender, EventArgs e)
+        {
+            int.TryParse(PenSizeBox.Text, out penradius);
         }
     }
 }
